890d0122179d517020a39977412c612b7f78a095
Student <student@Admins-Mac-mini.local>
2016-12-15 17:15:48 -0700

    string

---
+++ jave.js
@@ -0,0 +1,10 @@
+alert ('hello boulder');
+var word = prompt("Enter a word. Any word.");
+
+  alert(
+	"Your entered " + word + "\n" +
+	"There are " + word.length + " characters in the word." + "\n" +
+	"Lowercase: " + word.toLowerCase() + "\n" +
+	"Uppercase: " + word.toUpperCase() + "\n" +
+	"Example: I have wanted a " + word + " since I was a little boy." + "\n" +
+	"Subword:" + word.substring(1,4))
